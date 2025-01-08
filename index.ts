6c81beb92e73b7a03e5fa0265fc4fbf1ca4ef132
Attato <rde8519@mail.ru>
2025-01-09 02:39:10 +0300

    добавил компонент Input

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import Handlebars from "handlebars";
+import { Input } from "./src/components/Input";
+import { EventBus } from "./src/components/EventBus";
 
 interface Field {
 	label: string;
@@ -38,10 +40,41 @@ const authData: Context = {
 	grayButtonLink: "/src/pages/auth/signup.html",
 };
 
-const templateElement = document.getElementById("auth-template");
-const container = document.getElementById("auth-container");
+const templateSource = `
+    <main class="auth">
+        <form class="auth__wrap">
+            <h2 class="auth__title">{{title}}</h2>
+            
+            <div class="input__wrap" id="input-wrapper"></div>
 
-if (templateElement && container) {
-	const template = Handlebars.compile(templateElement.innerHTML);
-	container.innerHTML = template(authData);
+            <div class="auth__actions">
+                <a href="{{whiteButtonLink}}" class="button__white">{{whiteButton}}</a>
+                <hr>
+                <a href="{{grayButtonLink}}" class="button__gray">{{grayButton}}</a>
+            </div>
+        </form>
+    </main>
+`;
+
+const authElement = document.getElementById("auth");
+const eventBus = new EventBus();
+
+const template = Handlebars.compile(templateSource);
+const html = template(authData);
+
+if (authElement) {
+	authElement.innerHTML = html;
+
+	const inputWrapper = document.getElementById("input-wrapper");
+
+	if (inputWrapper) {
+		authData.fields.forEach((field) => {
+			const inputComponent = new Input(field, eventBus);
+			inputWrapper.appendChild(inputComponent.getElement());
+		});
+	}
+
+	eventBus.on("inputChange", (data: { name: string; value: string }) => {
+		console.log(`Поле ${data.name} изменено: ${data.value}`);
+	});
 }
