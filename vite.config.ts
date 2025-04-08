cd9fef6656bdb6f2e890aad1d02f0a9d47ee9c2a
Attato <rde8519@mail.ru>
2025-04-09 00:42:19 +0300

    мелочи

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
+	resolve: {
+		alias: {
+			pages: path.resolve(__dirname, "src/pages"),
+		},
+	},
 	build: {
 		rollupOptions: {
 			input: {
 				homePage: "/index.html",
-				signUp: "/src/pages/auth/signup.html",
-				chat: "/src/pages/chat/chat.html",
+				signUp: "/src/pages/auth/signup.ts",
+				chat: "/src/pages/chat/chat.ts",
 				page404: "/src/pages/error/404.html",
 				page500: "/src/pages/error/500.html",
 			},
