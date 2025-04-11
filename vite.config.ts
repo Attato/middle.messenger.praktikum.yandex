8d1c14bda7eb7411c281532f632a691a533d3e35
Attato <rde8519@mail.ru>
2025-04-11 13:17:56 +0300

    попытка починить cors

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,16 @@ import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
+	server: {
+		proxy: {
+			"/api": {
+				target: "https://ya-praktikum.tech",
+				changeOrigin: true,
+				secure: true,
+				rewrite: (path) => path.replace(/^\/api/, ""),
+			},
+		},
+	},
 	resolve: {
 		alias: {
 			pages: path.resolve(__dirname, "src/pages"),
