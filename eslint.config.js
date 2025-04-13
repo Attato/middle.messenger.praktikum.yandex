497f976ab28ab10c3109d9b858ad4a1c348331b1
Attato <rde8519@mail.ru>
2025-04-13 23:29:38 +0300

    добавил husky, chai, mocha, lint-staged и поправил некоторые конфиги

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,25 @@
+import tseslint from "@typescript-eslint/eslint-plugin";
+import parser from "@typescript-eslint/parser";
+
+/** @type {import("eslint").Linter.Config[]} */
 export default [
 	{
+		files: ["**/*.ts"],
+		languageOptions: {
+			parser,
+			parserOptions: {
+				ecmaVersion: "latest",
+				sourceType: "module",
+				project: "./tsconfig.json",
+			},
+		},
+		plugins: {
+			"@typescript-eslint": tseslint,
+		},
 		rules: {
 			semi: "error",
 			"prefer-const": "error",
+			...tseslint.configs.recommended.rules,
 		},
 	},
 ];
