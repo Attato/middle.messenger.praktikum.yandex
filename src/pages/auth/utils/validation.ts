caa8ee3d92c5f739a19bb0743ec34efb3fe044fd
Attato <rde8519@mail.ru>
2025-04-14 13:58:49 +0300

    добавил буквы ё

--- src/pages/auth/utils/validation.ts
+++ src/pages/auth/utils/validation.ts
@@ -1,12 +1,12 @@
 const validationRules: Record<string, (value: string) => string | null> = {
 	first_name: (value) => {
-		const regex = /^[A-ZА-Я][a-zа-я-]*$/;
+		const regex = /^[A-ZА-ЯЁ][a-zа-яё-]*$/;
 		return regex.test(value)
 			? ""
 			: "* Имя должно быть на латинице или кириллице, начинаться с заглавной буквы и не содержать пробелов, цифр или спецсимволов.";
 	},
 	second_name: (value) => {
-		const regex = /^[A-ZА-Я][a-zа-я-]*$/;
+		const regex = /^[A-ZА-ЯЁ][a-zа-яё-]*$/;
 		return regex.test(value)
 			? ""
 			: "* Фамилия должна быть на латинице или кириллице, начинаться с заглавной буквы и не содержать пробелов, цифр или спецсимволов.";
@@ -29,9 +29,7 @@ const validationRules: Record<string, (value: string) => string | null> = {
 	},
 	phone: (value) => {
 		const regex = /^\+?\d{10,15}$/;
-		return regex.test(value)
-			? ""
-			: "* Телефон должен содержать от 10 до 15 цифр и может начинаться со знака +.";
+		return regex.test(value) ? "" : "* Телефон должен содержать от 10 до 15 цифр и может начинаться со знака +.";
 	},
 	message: (value) => {
 		return value.trim() !== "" ? "" : "* Сообщение не должно быть пустым.";
